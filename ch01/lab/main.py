60bb2c5fa7e12ce9c722adbab5ffd8f1a322dff6
Anna <nnminh2810@gmail.com>
2023-01-30 17:08:37 -0500

    create data.py and make edits

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,1 +1,8 @@
-
+#Part A
+weeks = 16
+classes = 5
+tuition = 6000
+cost_per_week = ((tuition / classes) / weeks)
+print("Cost per week:", cost_per_week)
+classes_per_week = 3
+cost_per_class = cost_per_week/classes_per_week
