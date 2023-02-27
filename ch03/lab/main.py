4ee8e66f7f90aa444ae1f55409e4238c2e3b54cb
Anna Nguyen <82323965+anna-nm@users.noreply.github.com>
2023-02-27 13:01:47 -0500

    Update main.py

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -4,20 +4,10 @@ pygame.init()
 sf = pygame.display.set_mode()
 sf.fill("white")
 pygame.time.wait(1000)
+
 pygame.display.flip()
+pygame.draw.rect(sf,"blue",[500,500,500,500])
+pygame.draw.rect(sf,"blue",[500,50,500,500])
 
-
-pygame.draw.rect(sf,"blue",[100,100,100,100], 50)
 pygame.display.flip()
-pygame.time.import pygame
-pygame.init()
-
-sf = pygame.display.set_mode()
-sf.fill("white")
-pygame.time.wait(1000)
-pygame.display.flip()
-
-
-pygame.draw.rect(sf,"blue",[100,100,100,100], 50)
-pygame.display.flip()
-pygame.time.
+pygame.time.wait(2000)
