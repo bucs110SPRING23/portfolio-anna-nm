c910d0fa4863886d05b474cb38c911ae7e50cf90
Anna <nnminh2810@gmail.com>
2023-02-27 13:10:34 -0500

    update lab

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -4,9 +4,10 @@ pygame.init()
 sf = pygame.display.set_mode()
 sf.fill("white")
 pygame.time.wait(1000)
-pygame.display.flip()
 
-
-pygame.draw.rect(sf,"blue",[100,100,100,100], 50)
 pygame.display.flip()
-pygame.time.
+pygame.draw.rect(sf,"blue",[500,500,500,500])
+pygame.draw.rect(sf,"blue",[500,50,500,500])
+
+pygame.display.flip()
+pygame.time.wait(2000)
