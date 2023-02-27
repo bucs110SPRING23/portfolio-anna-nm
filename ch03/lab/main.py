354ecb8e4aa8c4d786c87ad5d99e6ef62ff13b01
Anna <nnminh2810@gmail.com>
2023-02-27 13:21:00 -0500

    update

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,9 +1,14 @@
 import pygame
 pygame.init()
 
+
+
+
 sf = pygame.display.set_mode()
+pygame.display.get_window_size()
+
 sf.fill("white")
-pygame.time.wait(1000)
+print(pygame.time.wait(1000))
 
 pygame.display.flip()
 pygame.draw.rect(sf,"blue",[500,500,500,500])
