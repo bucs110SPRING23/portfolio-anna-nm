1aba45dffec2546ecbe00a6d6e674f74f3ded3fd
Anna Nguyen <82323965+anna-nm@users.noreply.github.com>
2023-02-27 18:14:04 -0500

    Update main.py

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,18 +1,13 @@
 import pygame
 pygame.init()
 
-
-
-
-sf = pygame.display.set_mode()
-pygame.display.get_window_size()
+sf = pygame.display.set_mode((600,600))
 
 sf.fill("white")
 print(pygame.time.wait(1000))
 
 pygame.display.flip()
-pygame.draw.rect(sf,"blue",[500,500,500,500])
-pygame.draw.rect(sf,"blue",[500,50,500,500])
-
+pygame.draw.rect(sf,"blue",[0,0,600,600])
+pygame.draw.circle(sf, "pink", [300,300],300)
 pygame.display.flip()
-pygame.time.wait(2000)
+pygame.time.wait(10000)
