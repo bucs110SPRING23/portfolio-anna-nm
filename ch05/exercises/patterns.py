df37d010e67ccdf516a4792f2b43e86266a1cba2
Anna Nguyen <82323965+anna-nm@users.noreply.github.com>
2023-03-06 17:52:59 -0500

    Create patterns.py

---
+++ ch05/exercises/patterns.py
@@ -0,0 +1,10 @@
+n = input("Enter the number of rows: ")
+def star_pyramid(n):
+  for i in range(n): 
+    print("*" * (i+1))
+
+def rstar_pyramid(n)
+  for i in range(n, 0, -1): 
+    print("*" * (i+1))
+ 
+ 
