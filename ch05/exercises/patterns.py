40abf8af9ba0ef33f7bdf6572851ca2f6405ccaa
Anna <nnminh2810@gmail.com>
2023-03-08 16:32:11 -0500

    update

--- ch05/exercises/patterns.py
+++ ch05/exercises/patterns.py
@@ -1,10 +1,13 @@
-n = input("Enter the number of rows: ")
+n = int(input("Enter the number of rows: "))
 def star_pyramid(n):
   for i in range(n): 
     print("*" * (i+1))
 
-def rstar_pyramid(n)
+def rstar_pyramid(n):
   for i in range(n, 0, -1): 
-    print("*" * (i+1))
- 
- 
+    print("*" * i)
+
+#test
+star_pyramid(n)
+rstar_pyramid(n)
+
