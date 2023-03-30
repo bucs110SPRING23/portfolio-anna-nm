d87f86ba14a06605527f697c2fdc696395d7750a
Anna <nnminh2810@gmail.com>
2023-03-29 22:16:11 -0400

    update

--- ch07/lab/Rectangle.py
+++ ch07/lab/Rectangle.py
@@ -11,17 +11,9 @@ class Rectangle:
         self.height = abs(h)
         self.width = abs(w)
     def __str__(self): 
+        '''
+        return the value of the instance variables
+        '''
         return "x: " + str(self.x) + ", y: " + str(self.y) + ", height: " + str(self.height) + " ,width: " + str(self.width)
 
-#print(Rectangle(2,1,4,5))
-r = Rectangle(10, 10, 10, 10)
-assert((r.x, r.y, r.height, r.width) == (10,10,10,10))
-r = Rectangle(-1, 1, 1, 1)
-assert((r.x, r.y, r.height, r.width) == (1,1,1,1))
-r = Rectangle(1, -5, 1, 1)
-assert((r.x, r.y, r.height, r.width) == (1,5,1,1))
-r = Rectangle(1, 1, -10, 1)
-assert((r.x, r.y, r.height, r.width) == (1,1,10,1))
-r = Rectangle(1, 1, 1, -1000)
-assert((r.x, r.y, r.height, r.width) == (1,1,1,1000))
 
