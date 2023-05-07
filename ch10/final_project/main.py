116e7189c1e0f9c6b269cc54f483f73d0f3e236b
Anna <nnminh2810@gmail.com>
2023-05-07 18:05:44 -0400

    error when calling covid19.py

--- ch10/final_project/main.py
+++ ch10/final_project/main.py
@@ -1,16 +1,22 @@
-import pygame
-
-# import your controller
-
+import random
+from src import covid19
+from src import ipaddress
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def main():
-    pygame.init()
-    # Create an instance on your controller object
-    # Call your mainloop
-
-    ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
-
-
-# https://codefather.tech/blog/if-name-main-python/
+    ip = str(input("Input the IP or type in 0 to generate a random one: "))
+    if ip == '0': 
+        random_ip = ""
+        for i in range(4): 
+            random_ip += str(random.randint(0, 255))
+            random_ip += "."
+        random_ip = str(random_ip[:-1])
+        country = ipaddress.IPtoCountry(random_ip).get()
+        print(covid19.Covid19(country))
+    else: 
+        country = ipaddress.IPtoCountry(str(ip)).get()
+        print(covid19.Covid19(country))
+    
 if __name__ == "__main__":
     main()
--- ch10/final_project/src/ipaddress.py
+++ ch10/final_project/src/ipaddress.py
@@ -1,11 +1,20 @@
-import random 
 import requests
+import json
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-class IPaddress: 
-    def __init__(self, ip) -> None:
+class IPtoCountry: 
+    def __init__(self, ip):
         self.api_url = f"https://ipvigilante.com/json/{ip}"
 
     def get(self): 
-        r = requests.get(self.api_url)
-        response = r.json()
+        r = requests.get(self.api_url, verify=False)
+        if r.status_code == 200:
+            response = r.json()
+            country = str(response['data']['country_name'])
+            print(country)
+            return country
+        else:
+            print("Error from server: " + str(r.content))
+        #response = r.json()
         
